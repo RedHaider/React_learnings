5137062e0bb13fd7d2eaf59c81fa3d2c4bb62e24
RedHaider <syedaliredwanulhaider13@gmail.com>
2024-06-04 17:05:39 +0600

    s2

---
+++ demo-project/src/Blogs.js
@@ -0,0 +1,24 @@
+const Blogs = (props ) => {
+const list = props.list;
+const title = props.title;
+
+
+    return ( 
+        <div>
+         <h1>{title}</h1>
+        {list.map((item)=>(
+            <div key={item.id} className="post">
+                <h3>{item.title}</h3>
+                <p>{item.body}</p>
+                <p>{item.author}</p>
+                <p><strong>{item.id}</strong></p>
+            </div>
+        ))}
+        </div>
+     );
+}
+ 
+export default Blogs;
+
+
+// <button onClick={()=>deleteButton(item.id)}>Delete</button> 
--- demo-project/src/Home.js
+++ demo-project/src/Home.js
@@ -1,22 +1,78 @@
+import { useState , useEffect } from "react";
+import Blogs from "./Blogs";
+
 const Home = () => {
 
-    const handleClick = ()=> {
-        console.log("He Clicked");
-    }
 
-    const againclick = (name)=> {
-        console.log('hello'+name);
-    }
+const [list, setList] = useState(null);
+const [isPending , setisPending]= useState(true);
+const [error, setError] = useState(null)
 
+
+useEffect(()=>{
+    fetch('http://localhost:8000/list')
+    .then(res=> {
+        if(!res.ok){
+            throw Error('Could not fetch the data for that resourse');
+        }
+        return res.json();
+    }).then(data => {
+        setList(data);
+        setisPending(false);
+        setError=(null)
+    }).catch(err=> {
+        setisPending(false);
+        setError(err.message);
+    })
+},[]);
 
     return ( 
         <div className="home">
             <h2>Home Page</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={()=>againclick('Haider')}>He Clicked again</button>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {list && <Blogs list={list} title="Title" />}
         </div>
 
      );
-}
- 
+
+};
 export default Home;
+
+// setting the time to delay the execution
+// setTimeout(()=>{
+//     fetch('http://localhost:8000/list').then(res=> {
+//         return res.json();
+//     }).then(data => {
+//         setList(data);
+//         setisPending(false);
+//     })
+//  },1000);
+
+
+// const deleteButton = (id) => {
+//     const newlist = list.filter(list => list.id != id);
+//     setList(newlist);
+// }
+
+
+// <Blogs list={list.filter((list) =>
+//     list.author=='Alice'
+//     )}
+//  title= 'Filtered'
+// />
+
+
+//     const handleClick = ()=> {
+//         console.log("He Clicked");
+//     }
+
+// const name_1 = 'Mario';
+// const age_1 = 30
+// const [name, setName] = useState(name_1)
+// const [age, setAge] = useState(age_1)
+
+// const handleClick = () => {
+//     setName('Haider');
+//     setAge(40);
+// }
