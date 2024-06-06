7512a17d60dd48d402a625aa42db2ace2e2a3786
RedHaider <syedaliredwanulhaider13@gmail.com>
2024-06-06 09:52:05 +0600

    s6

--- demo-project/src/Create.js
+++ demo-project/src/Create.js
@@ -4,7 +4,16 @@
 const Create = () => {
     return ( 
         <div className="create">
-            <h1>This is create page</h1>
+            <h1>Create a Blog Record</h1>
+            <form>
+                <label>Title:</label>
+                <input type="text" required/>
+                <label>Body:</label>
+                <textarea>
+                </textarea>
+                <label>Author:</label>
+                <input type="text" required/>
+            </form>
         </div>
      );
 }
